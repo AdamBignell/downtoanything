f91cb3cc256e49ac42b1ad1bda6eddf0c19eb8d4
nadiaciobanu <nadiac123@gmail.com>
2016-11-06 17:43:47 -0800

    modified themes

--- db/schema.rb
+++ db/schema.rb
@@ -16,16 +16,6 @@ ActiveRecord::Schema.define(version: 20161103004409) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "abouts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "admins", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "challenges", force: :cascade do |t|
     t.string   "user_id"
     t.string   "name"
@@ -34,17 +24,20 @@ ActiveRecord::Schema.define(version: 20161103004409) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "contacts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "employee", id: false, force: :cascade do |t|
+    t.string "name",     limit: 20
+    t.string "dept",     limit: 20
+    t.string "jobtitle", limit: 20
   end
 
-  create_table "guards", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "employee_with_number", id: false, force: :cascade do |t|
+    t.string  "name", limit: 20
+    t.integer "cell"
   end
 
-  create_table "logins", force: :cascade do |t|
+  create_table "people", force: :cascade do |t|
+    t.string   "pname"
+    t.integer  "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -58,7 +51,50 @@ ActiveRecord::Schema.define(version: 20161103004409) do
     t.datetime "updated_at",   null: false
   end
 
-  create_table "supervisors", force: :cascade do |t|
+  create_table "things", force: :cascade do |t|
+    t.string   "tname"
+    t.text     "description"
+    t.integer  "person_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "tokemons", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "weight"
+    t.integer  "height"
+    t.integer  "fly"
+    t.integer  "fight"
+    t.integer  "fire"
+    t.integer  "water"
+    t.integer  "electrify"
+    t.integer  "ice"
+    t.integer  "total"
+    t.integer  "trainer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tokimons", force: :cascade do |t|
+    t.string   "name"
+    t.float    "weight"
+    t.float    "height"
+    t.integer  "fly"
+    t.integer  "fight"
+    t.integer  "fire"
+    t.integer  "water"
+    t.integer  "electric"
+    t.integer  "ice"
+    t.integer  "total"
+    t.integer  "trainer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "bodytype"
+  end
+
+  create_table "trainers", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "level"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
