b78b8f974bfa12daf54d13f1e4e763d2433c9f60
Matt <matalrudenko@gmail.com>
2016-11-07 21:05:31 -0800

    fixed error with new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :confirm_logged_in, :except => [:new]
 
   # GET /users
   # GET /users.json
