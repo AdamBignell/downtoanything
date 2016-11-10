ab1bfa79ad96fc8701fc8e51f111b29ebf93fc02
nadiaciobanu <nadiac123@gmail.com>
2016-11-09 16:01:46 -0800

    schema problems

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107022200) do
+ActiveRecord::Schema.define(version: 20161108001329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(version: 20161107022200) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "url"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "points"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "things", force: :cascade do |t|
@@ -104,11 +111,11 @@ ActiveRecord::Schema.define(version: 20161107022200) do
     t.string   "email"
     t.string   "username"
     t.integer  "points"
-    t.string   "team"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.integer  "team_id"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
-    t.boolean  "admin"
+    t.boolean  "admin",           default: false, null: false
   end
 
   create_table "widgets", force: :cascade do |t|
