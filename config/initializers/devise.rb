415b1dca8a1afbfb5937e6b4e1f4b16eadfd3a69
Ryan Scovill <ryans797@gmail.com>
2016-11-15 22:37:42 -0800

    youtube api scope

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -248,7 +248,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   require 'omniauth-google-oauth2'
-  config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {scope: 'email, profile'}
+  config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {scope: 'email, profile, youtube, youtube.upload', access_type: 'online'}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
