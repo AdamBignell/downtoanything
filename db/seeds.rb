1c11d572956cfe911f1f7017b36c5da96c42a676
AdamBignell <adambignell@gmail.com>
2016-11-20 22:03:56 -0800

    Make seeds that do not require confirmation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+include Devise
 
 u = User.create(email: 'admin@dta.ca', username: 'admin', points: 0, team_id: 1, password: 'adminpass', admin: true)
-u.save
+u.skip_confirmation!
+u.save!
+
+u2 = User.create(email: 'abignell@sfu.ca', username: 'malign_blade', points: 0, team_id: 1, password: 'gandalf18', admin: true)
+u2.skip_confirmation!
+u2.save!
+
+c = Challenge.create(name: "Film a mythical beast!", description: 'Film an animal that nobody has every filmed before!')
+c.update_attribute(:user_id, u2.id)
+c.save!
--- test/controllers/challenges_controller_test.rb
+++ test/controllers/challenges_controller_test.rb
@@ -4,43 +4,35 @@ class ChallengesControllerTest < ActionController::TestCase
 
   # The below lines fix the Devise 'could not find 'Warden:Proxy'' error
   include Devise::Test::ControllerHelpers
-  include Devise::TestHelpers                          
-  include Warden::Test::Helpers                        
-  Warden.test_mode!                                    
+  # include Devise::TestHelpers                          
+  # include Warden::Test::Helpers                        
+  # Warden.test_mode!                                    
 
-  def teardown                                         
-    Warden.test_reset!                                 
-  end
+  # def teardown                                         
+  #   Warden.test_reset!                                 
+  # end
 
   setup do
+    @request.env['devise.mapping'] = Devise.mappings[:challenge]
     @challenge = challenges(:one)
     @challenge.save!
+    User.first.confirm!
+    sign_in User.first
   end
 
-  test "should show challenges" do
-    sign_in User.first
-    get :index
-    Challenge.all.each do |challenge|
-      assert_select "span", challenge.name
-    end
-  end
 
   test "should get index" do
-    sign_in User.first
     get :index
     assert_response :success
     assert_not_nil assigns(:challenges)
   end
 
   test "should get new" do
-    @request.env['devise.mapping'] = Devise.mappings[:challenge]
-    sign_in User.first
     get :new
     assert_response :success
   end
 
   test "should create challenge" do
-    sign_in User.first
     assert_difference('Challenge.count') do
       post :create, challenge: { name: @challenge.name, score: @challenge.score, user_id: @challenge.user_id }
     end
@@ -49,25 +41,21 @@ class ChallengesControllerTest < ActionController::TestCase
   end
 
   test "should show challenge" do
-    sign_in User.first
     get :show, id: @challenge
     assert_response :success
   end
 
   test "should get edit" do
-    sign_in User.first
     get :edit, id: @challenge
     assert_response :success
   end
 
   test "should update challenge" do
-    sign_in User.first
     patch :update, id: @challenge, challenge: { name: @challenge.name, score: @challenge.score, user_id: @challenge.user_id }
     assert_redirected_to challenge_path(assigns(:challenge))
   end
 
   test "should destroy challenge" do
-    sign_in User.first
     assert_difference('Challenge.count', -1) do
       delete :destroy, id: @challenge
     end
