23c85f133a299205f5f9e306cb811783d5e260c2
AdamBignell <adambignell@gmail.com>
2016-11-20 18:31:32 -0800

    Add non-nil tests to challenge objects

--- test/models/challenge_test.rb
+++ test/models/challenge_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class ChallengeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+test "challenge created properly" do
+  	c = Challenge.create(name: 'Say Alphabet Backwards', description: 'Recite the alphabet from Z to A')
+	c.save
+	assert_not_nil(c, "Challenges are not properly created!")
+	end
 end
