6dc47110813736539399829b91b447671da5ee74
AdamBignell <adambignell@gmail.com>
2016-11-20 23:15:30 -0800

    Added one more test to challenges to ensure they are actually attributed to users

--- test/models/challenge_test.rb
+++ test/models/challenge_test.rb
@@ -7,12 +7,13 @@ test "challenge created properly" do
 	c.save
 	assert_not_nil(c, "Challenges are not properly created!")
 	end
-end
+
 
 test "challenge attributed properly" do
   	c = Challenge.create(name: 'Make the funniest face!', description: 'Make the most hilarious face you can possibly make!')
 	c.save
 	u = User.first
-	assert_not_nil(c, "Challenges are not properly created!")
+	u.challenges << c
+	assert_includes(u.challenges, c, "Challenges are not attributed correctly!")
 	end
 end
