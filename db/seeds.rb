1401b750a69a00e36aaaede4a5e685da24280889
AdamBignell <adambignell@gmail.com>
2016-11-20 22:28:36 -0800

    Add additional seed to reflect requirements document

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,14 @@ u2 = User.create(email: 'abignell@sfu.ca', username: 'malign_blade', points: 0, 
 u2.skip_confirmation!
 u2.save!
 
+u3 = User.create(email: 'tommyp@gravity.net', username: 'TommyP', points: 0, team_id: 1, password: 'slothrop', admin: true)
+u3.skip_confirmation!
+u3.save!
+
+u4 = User.create(email: 'haroldg@sfu.ca', username: 'OldHarold', points: 0, team_id: 1, password: 'letmein', admin: true)
+u4.skip_confirmation!
+u4.save!
+
 c = Challenge.create(name: "Film a mythical beast!", description: 'Film an animal that nobody has every filmed before!')
 c.update_attribute(:user_id, u2.id)
 c.save!
