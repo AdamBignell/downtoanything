cf09f91f41592270f80c28eda8d07388b2a9feab
Ryan Scovill <ryans797@gmail.com>
2016-11-21 19:29:08 -0800

    SendGrid email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  ActionMailer::Base.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 587,
+    domain: "downtoanything.com",
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD']
+  }
 end
