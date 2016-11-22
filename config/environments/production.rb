287775004fe4b00ef776137f7e135579c3d92269
Ryan Scovill <ryans797@gmail.com>
2016-11-21 19:32:33 -0800

    Mailer settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { :host => 'downtoanything.com' }
 
   ActionMailer::Base.smtp_settings = {
     address: "smtp.sendgrid.net",
