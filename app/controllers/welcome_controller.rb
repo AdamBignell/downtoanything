1a99c9875456cf3cfb2589de73b6a0dcdbf638c6
AdamBignell <adambignell@gmail.com>
2016-11-30 12:20:41 -0800

    Add top submission to homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,15 @@ class WelcomeController < ApplicationController
 
   # GET /welcome
   def index
-
+    @submissions = Submission.all
+    @subs = @submissions.sort_by { |sub| sub.score}
+    if not @subs.blank?
+      @topsub = @subs[0]
+    end
+    @challenge = Challenge.find(@topsub.challenge_id)
+    @users = User.all
+    @comments = Comment.all
+    @interaction = UserInteraction.where(:user_id => current_user.id, :submission_id => @topsub.id).first
   end
 
 end
