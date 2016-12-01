6afa58a907b7862a2f2773567aba598c34bed978
AdamBignell <adambignell@gmail.com>
2016-11-30 19:08:32 -0800

    Bug fixes, made top submission properly display

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,7 +7,10 @@ class WelcomeController < ApplicationController
     if not @subs.blank?
       @topsub = @subs[0]
       @challenge = Challenge.find(@topsub.challenge_id)
-      @interaction = UserInteraction.where(:user_id => current_user.id, :submission_id => @topsub.id).first
+      if user_signed_in?
+        @user = current_user
+        @interaction = UserInteraction.where(:user_id => current_user.id, :submission_id => @topsub.id).first
+      end
     end
     @users = User.all
     @comments = Comment.all
