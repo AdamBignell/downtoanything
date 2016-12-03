9addb46692f81e18116840f56691c8d3c45af220
AdamBignell <adambignell@gmail.com>
2016-12-03 14:57:25 -0800

    Fix dangling bodies and titles

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :submission
   belongs_to :user
 
-  validates_length_of :body, :maximum => 500
+  validates_length_of :bodytext, :maximum => 500
   validates :user_id, :presence => :true
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
 	test 'has user_id' do
-		comment = Comment.new(body: 'Toil and Trouble')
+		comment = Comment.new(bodytext: 'Toil and Trouble')
 		refute comment.valid?
 		assert_not_nil comment.errors[:user_id]		
 	end
