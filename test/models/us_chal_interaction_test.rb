123ca08d647bdd54eb3c8eb5e398cc10ac024285
Matt <matalrudenko@gmail.com>
2016-12-03 16:51:04 -0800

    Model tests for user_interactions added. That's a professional commit message, right, Ryan?

--- test/models/us_chal_interaction_test.rb
+++ test/models/us_chal_interaction_test.rb
@@ -4,4 +4,10 @@ class UsChalInteractionTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "must have interaction set" do
+    ui = UsChalInteraction.new
+    refute ui.valid?
+    assert_not_nil ui.errors
+  end
 end
