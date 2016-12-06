b35df05d2da11b923e0b541db22e79fd9fb4a711
Matt <matalrudenko@gmail.com>
2016-12-06 14:39:15 -0800

    Added CSRF authorization to destroy method for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -112,6 +112,7 @@ class UsersController < ApplicationController
   # DELETE /users/1.json
   def destroy
     if is_admin?
+      @user = User.find(params[:id])
       @user.destroy
       respond_to do |format|
         format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
